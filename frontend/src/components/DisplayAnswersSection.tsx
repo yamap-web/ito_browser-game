b171686ee81afa12c10400340444c03b2055220f
yamap <yamap.web@gmail.com>
2024-06-03 15:42:13 +0900

    💥 react-beautiful-dndの導入、ドラッグアンドドロップの実装

--- frontend/src/components/DisplayAnswersSection.tsx
+++ frontend/src/components/DisplayAnswersSection.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import type { GameData } from "../interfaces/interface";
 
 const DisplayAnswersSection = () => {
@@ -14,30 +16,75 @@ const DisplayAnswersSection = () => {
   ];
   //#endregion
 
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 1024);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth > 1024);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const reorder = (list: GameData[], startIndex: number, endIndex: number) => {
+    const removed = list.splice(startIndex, 1);
+    list.splice(endIndex, 0, removed[0]);
+  };
+
+  const onDragEnd = (result: any) => {
+    if (!result.destination) return;
+    reorder(members, result.source.index, result.destination.index);
+  };
+
   return (
-    <ul className="flex flex-col lg:flex-row items-center justify-center w-full my-6">
-      {members
-        .sort((a, b) => a.index - b.index)
-        .map((member, index) => {
-          return <AnswerCard member={member} key={index} />;
-        })}
-    </ul>
+    <>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable
+          droppableId="droppable"
+          direction={isLargeScreen ? "horizontal" : undefined}
+        >
+          {(provided) => (
+            <ul
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              className="flex flex-col lg:flex-row items-center justify-center w-full my-6"
+            >
+              {members.map((member, index) => (
+                <Draggable
+                  key={member.userName}
+                  draggableId={member.userName}
+                  index={index}
+                >
+                  {(provided) => (
+                    <li
+                      className="card bg-base-200 flex items-center rounded-2xl border border-slate-100 shadow-md w-full lg:w-fit mt-2 lg:mt-0 lg:ml-2"
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      <AnswerCard member={member} key={index} />
+                    </li>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </>
   );
 };
 
 const AnswerCard = (props: { member: GameData }) => {
   return (
-    <li
-      draggable="true"
-      className="card bg-base-200 flex items-center rounded-2xl border border-slate-100 shadow-md w-full lg:w-fit mt-2 lg:mt-0 lg:ml-2"
-    >
-      <div className="card-body px-5 py-1 lg:py-4">
-        <span className="">{props.member.userName}</span>
-        <h2 className="card-title text-xl lg:text-2xl">
-          {props.member.answer}
-        </h2>
-      </div>
-    </li>
+    <div className="card-body px-5 py-1 lg:py-4">
+      <span className="">{props.member.userName}</span>
+      <h2 className="card-title text-xl lg:text-2xl">{props.member.answer}</h2>
+    </div>
   );
 };
 
