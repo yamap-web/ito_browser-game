b5502e528e63f7c84d50fcfce1e1da84d24874f0
akira0716 <akira.inaji.0716@gmail.com>
2024-06-09 16:29:25 +0900

    ✨ イベント受信時ログ出力機能追加

--- backend/app.ts
+++ backend/app.ts
@@ -1,12 +1,15 @@
 import type { Socket } from "socket.io";
 import { io } from "./socket";
 import eventRcv from "./utils/eventRcv";
+import SocketEvent from "./class/SocketEvent";
+import { outputEventLog } from "./utils/outputLog";
+import { LogLevel } from "./class/LogClass";
 
 const port = 3000;
 
 // 通信接続時処理
-io.on("connection", (socket: Socket) => {
-  console.log("User connected");
+io.on(SocketEvent.CONNECTION, (socket: Socket) => {
+  outputEventLog(LogLevel.INFO, socket.id, SocketEvent.CONNECTION);
 
   // イベント受信時処理登録
   eventRcv(socket);
