12212a31b11dbb557718d08a13c24b698f1e9112
yamap <yamap.web@gmail.com>
2024-06-12 19:09:18 +0900

    test

--- frontend/webpack.prod.ts
+++ frontend/webpack.prod.ts
@@ -3,7 +3,14 @@ import { merge } from "webpack-merge";
 import common from "./webpack.common";
 
 const prod: Configuration = merge(common, {
-  mode: "production",
+  mode: "development",
+  devServer: {
+    static: {
+      directory: "./dist",
+    },
+    historyApiFallback: true,
+    port: 8080,
+  },
 });
 
 export default prod;
