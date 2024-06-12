dbfad138e9f4ffd362cfd70cd1d0c1a8266af84f
yamap <yamap.web@gmail.com>
2024-06-12 16:45:54 +0900

    ✨ css関連の修正

--- frontend/postcss.config.ts
+++ frontend/postcss.config.ts
@@ -1,10 +1,10 @@
-import { Config } from "postcss-load-config";
+import type { Config } from "postcss-load-config";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import cssnano from "cssnano";
 
 const config: Config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [tailwindcss(), autoprefixer(), cssnano()],
 };
 
 export default config;
